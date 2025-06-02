297c1e9d0238d7ca4acf4a5fa160b7d9922288d1
md-suhail-git <mdsuhail1502@gmail.com>
2025-06-02 23:04:05 +0530

    git commit -m "firstcommit"

---
+++ ADONET/WebForm2.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ADONET
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from Employees", conn);
+                conn.Open();
+                GridView2.DataSource = cmd.ExecuteReader();
+                GridView2.DataBind();
+            }
+
+        }
+    }
+}
