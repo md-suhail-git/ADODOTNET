67aeae0a032ce34620c46bff402740d53b00a27e
md-suhail-git <mdsuhail1502@gmail.com>
2025-06-02 22:55:13 +0530

    Add project files.

---
+++ ADONET/WebForm1.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ADONET
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using(SqlConnection conn = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from EmpTable",conn);
+                conn.Open();
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+
+            }
+        }
+    }
+}
